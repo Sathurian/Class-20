ec4566cd0332c92cded963c11ba6bddd0f417e52
Sathurian <77487450+Sathurian@users.noreply.github.com>
2021-03-02 17:50:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var FR, MR;
+
+
+function setup() {
+  createCanvas(1200,800);
+ FR = createSprite(200,200,50,80);
+ FR.shapeColor = "green";
+ MR = createSprite(400,200,80,30);
+ MR.shapeColor = "green";
+}
+
+function draw() {
+  background(255,255,255);  
+  MR. x = mouseX;
+  MR.y = mouseY;
+  console.log(MR.x-FR.x);
+  if(MR.x-FR.x<FR.width/2+MR.width/2&& FR.x-MR.x<FR.width/2+MR.width/2&&FR.y-MR.y<FR.height/2+MR.height/2&&
+    MR.y-FR.y<MR.height/2+FR.height/2){
+    MR.shapeColor = "red"
+    FR.shapeColor = "red"
+  }
+  else{
+    MR.shapeColor = "green"
+    FR.shapeColor = "green"
+  }
+  drawSprites();
+}
